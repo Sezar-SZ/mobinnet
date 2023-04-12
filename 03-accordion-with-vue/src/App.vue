4ab99adae1ff2951d704ba71eb2ab9baeca7b59d
sezar <soheilzarepoor79@gmail.com>
2023-04-12 19:35:35 +0330

    Add Accordion wit Vue

---
+++ 03-accordion-with-vue/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <div id="faqs-container" dir="rtl">
+            <MessageItem
+                v-for="qa in data"
+                :qa="qa"
+                :key="qa.id"
+                :is-expanded="expandedID === qa.id"
+                @messageClicked="expandedID = $event"
+            ></MessageItem>
+        </div>
+    </div>
+</template>
+
+<script>
+import MessageItem from "./components/MessageItem.vue";
+import QA from "./assets/data.json";
+
+export default {
+    name: "App",
+    components: {
+        MessageItem,
+    },
+    data: function () {
+        return {
+            data: null,
+            expandedID: null,
+        };
+    },
+    mounted: function () {
+        this.data = QA;
+    },
+};
+</script>
+
+<style>
+@font-face {
+    font-family: "iransans";
+    src: url("./assets/iransansweb.woff");
+}
+
+* {
+    font-family: "iransans";
+}
+
+body {
+    background-color: #cec;
+}
+
+#faqs-container {
+    margin: 0 auto;
+    margin-top: 20vh;
+    padding: 2vh 5vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ 03-accordion-with-vue/src/components/MessageItem.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="faq">
+        <div
+            class="question"
+            :class="{ 'question-selected': isExpanded }"
+            @click="$emit('messageClicked', !isExpanded ? qa.id : null)"
+        >
+            <span>{{ qa.question }}</span>
+            <img
+                :class="{ rotate: isExpanded }"
+                src="../assets/down-arrow.svg"
+            />
+        </div>
+        <div class="answer" :class="{ expand: isExpanded }">
+            <span>{{ qa.answer }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MessageItem",
+    props: ["qa", "isExpanded"],
+};
+</script>
+
+<style scoped>
+.faq {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: start;
+    width: 90%;
+    max-width: 1200px;
+    margin-bottom: 4vh;
+}
+
+.question {
+    position: relative;
+    width: 100%;
+    padding: 2vh 2vw;
+    border: solid 1px gray;
+    border-radius: 2px;
+    background-color: #dfd;
+    cursor: pointer;
+}
+
+.question-selected {
+    background-color: #080;
+    color: white;
+}
+
+img {
+    width: 20px;
+    position: absolute;
+    left: 2vw;
+    transition: 200ms;
+}
+
+.rotate {
+    transform: rotate(180deg);
+}
+
+.answer {
+    padding: 2vh 2vw;
+    width: 100%;
+    border: solid 1px gray;
+    border-radius: 2px;
+    background-color: #dfd;
+    border-top: none;
+    color: #444;
+    display: none;
+}
+
+.expand {
+    display: block;
+}
+</style>
